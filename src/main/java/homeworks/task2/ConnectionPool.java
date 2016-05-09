0a26b02b11266281098db7f94134111af0643598
MsShy <tanyat93@mail.ru>
2016-05-09 12:43:18 +0100

    commit

--- src/main/java/homeworks/task2/ConnectionPool.java
+++ src/main/java/homeworks/task2/ConnectionPool.java
@@ -1,6 +1,5 @@
 package homeworks.task2;
 
-import javax.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,7 +7,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class ConnectionPool {
-
+	
 	private BlockingQueue<Connection> available;
 	private BlockingQueue<Connection> used;
 	private final String url;
@@ -17,58 +16,63 @@ public class ConnectionPool {
 	private final static int CONNECTION_SIZE = 8;
 	private Connection connection;
 
-
+	
 	public ConnectionPool(final String url, final String user, final String password) {
 		this.url = url;
 		this.user = user;
 		this.password = password;
-
-
+		
+		
 		try {
 			Class.forName("org.h2.Driver");
 			used = new ArrayBlockingQueue<Connection>(CONNECTION_SIZE);
 			available = new ArrayBlockingQueue<Connection>(CONNECTION_SIZE);
 			for (int index = 0; index < CONNECTION_SIZE; index++) {
-				connection = getConnection();
+				connection = DriverManager.getConnection(url, user, password);
 				PooledConnection pooledConnection = new PooledConnection(connection, this);
 				available.add(pooledConnection);
+
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}
-	}
-
-	public Connection getConnection() {
-		try {
-			return DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
-	}
-
-	public Connection takeConnection() {
-		connection = null;
-		try {
-			connection = available.take();
-			used.add(connection);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		return connection;
 	}
 	
+	public Connection getConnection() throws InterruptedException {
+		PooledConnection pooledConnection = null;
+		if (available != null) {
+			pooledConnection = (PooledConnection) available.take();
+			used.add(pooledConnection);
+		}
+
+		return pooledConnection;
+		
+	}
+	
+
 	public void giveBackConnection(PooledConnection connection) {
 
-		if (connection!=null) {
+		if (connection != null) {
 			used.remove(connection);
 			available.add(connection);
 		}
 	}
+	
+	public void close() {
+
+		for (Connection connection : used) {
+
+		}
+		used = null;
+		available = null;
 
 
+	}
 
 
+}
+	
 }
 
